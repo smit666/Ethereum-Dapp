a8c640b7d0d5b216752becda6a3f63bb142ab924
smit666 <smitthakur666@gmail.com>
2018-04-12 18:37:02 +0530

    changes

--- truffle.js
+++ truffle.js
@@ -3,12 +3,12 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "45.64.195.251",
+      host: "*",
       port: 8546,
       network_id: "*" // Match any network id
     },
     live: {
-      host: "45.64.195.251",
+      host: "*",
       port: 8546,
       network_id: "*",// Match any network id
       gas:"2000000"
